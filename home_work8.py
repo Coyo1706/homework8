bcd8a1117a3ef703ae7d381b2c671f7a8b271d0d
Coyo <artemprishva@gmail.com>
2022-12-09 01:53:16 +0200

    ver 0.2

--- home_work8.py
+++ home_work8.py
@@ -8,7 +8,7 @@ users = [{'Nikola Tesla': '1856/12/13'},
          {'Albert Einstein': '1879.12.15'},
          {'Alain Aspect': '1947,12,15'},
          {'Maryna Viazovska': '02-12-1984'},
-         {'Mr. Incognito': '7 Dec 2000'},
+         {'Mr. Incognito': '16 Dec 2000'},
          {'Mr. X': '1999 12 17'}
          ]
 
@@ -27,5 +27,14 @@ end_week = current_date + dt.timedelta(days=7)
 
 for name, date in users.items():
     if current_date <= date <= end_week:
-        birthday_per_week[date.strftime('%A')] = name
+        if date.weekday() == 5:
+            date = date + dt.timedelta(days=2)
+            birthday_per_week[date.strftime('%A')] = name
         print(birthday_per_week)
+        if date.weekday() == 6:
+            date = date + dt.timedelta(days=1)
+            birthday_per_week[date.strftime('%A')] = name
+        else:
+            birthday_per_week[date.strftime('%A')] = name
+
+
