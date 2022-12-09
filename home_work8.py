cd610bf6833a292caad53910a2a1ba5a72b08567
Coyo <artemprishva@gmail.com>
2022-12-09 09:41:34 +0200

    ver 0.4

--- home_work8.py
+++ home_work8.py
@@ -1,23 +1,23 @@
 import datetime as dt
 import dateparser
+from collections import defaultdict
 
 
-birthday_per_week = {}
+birthday_per_week = defaultdict(list)
 
-users = [{'Nikola Tesla': '1856/12/10'},
-         {'Albert Einstein': '1879.12.11'},
-         {'Alain Aspect': '1947,12,15'},
-         {'Maryna Viazovska': '02-12-1984'},
-         {'Mr. Incognito': '16 Dec 2000'},
-         {'Mr. X': '1999 12 17'},
-         {'Mr. y': '1999 12 14'}
+users = [{'Nikola Tesla': '1856/12/10'},{'Albert Einstein': '1879.12.11'}, {'Alain Aspect': '1947,12,15'},
+         {'Maryna Viazovska': '12-12-1984'}, {'Mr. Incognito': '16 Dec 2000'},  {'Mr. X': '1999 12 17'},
+         {'Mr. y': '1999 12 14'}, {"Yulia": "06/12/1996"}, {'Yarpl': '07/12/1997'}, {'Sunny': '26/12/2000'},
+         {'Luk': '10/12/1995'}, {'Sim': '12/12/1991'}, {'Nok': '14/12/1994'}, {'Kred': '13/12/1994'},
+         {'Tim': '18/12/1997'}, {'Kooc': '09/12/1997'}
          ]
+
 
 users = {k: v for d in users for k, v in d.items()}
 users = {k: dateparser.parse(v) for k, v in users.items()}
 users = {k: v.replace(year=dt.date.today().year) for k, v in users.items()}
 
-current_date = dt.datetime.today()
+current_date = dt.date.today()
 
 while current_date.weekday() < 5:
     current_date = current_date + dt.timedelta(days=1)
@@ -25,19 +25,25 @@ if current_date.weekday() == 6:
     current_date = current_date - dt.timedelta(days=1)
 
 end_week = current_date + dt.timedelta(days=6)
-print(current_date, end_week)
 
-for name, date in users.items():
-    if current_date <= date <= end_week:
-        if date.weekday() == 5:
-            date = date + dt.timedelta(days=2)
-            birthday_per_week[date.strftime('%A')] = name
 
-        if date.weekday() == 6:
-            date = date + dt.timedelta(days=1)
-            birthday_per_week[date.strftime('%A')] = name
+for name, dates in users.items():
+
+    if current_date <= dates.date() <= end_week:
+
+        if 0 < dates.weekday() < 5:
+            birthday_per_week[dates.strftime('%A')].append(name)
+
+        elif dates.weekday() == 5:
+            dates = dates + dt.timedelta(days=2)
+            birthday_per_week[dates.strftime('%A')].append(name)
+
         else:
-            birthday_per_week[date.strftime('%A')] = name
+            dates = dates + dt.timedelta(days=1)
+            birthday_per_week[dates.strftime('%A')].append(name)
+
 print(birthday_per_week)
+
+
 
 
